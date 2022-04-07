3cc3e7fd84ee1ccfb97910aadb1526e9027454c1
Jesse Woods <jessehwoods@gmail.com>
2022-04-07 10:20:06 -0400

    Moved side to a separate class again because I'm indecisive.

--- TorchAndBridgePuzzleSolver/src/problem/Problem.java
+++ TorchAndBridgePuzzleSolver/src/problem/Problem.java
@@ -99,39 +99,5 @@ public class Problem {
 		START_SIDE,
 		TARGET_SIDE;
 	}
-	
-	/**
-	 * Holds information on the sides of the bridge and methods allow the movers to be moved from one
-	 * Side to another.
-	 * Movers on a side are stored as ints representing their speed. 
-	 * Position in the array is used to distinguish different movers with the same speed. 
-	 * 
-	 * @author Jesse Woods
-	 *
-	 */
-	private class Side {
-		
-		/**
-		 * Array holding the movers that are on this side of the bridge. Int value represents 
-		 * the speed of the mover. Kept in ascending order.
-		 */
-		private List<Integer> movers;
-		
-		/**
-		 * Default constructor that creates an empty Side.
-		 */
-		public Side() {
-			this.movers = new ArrayList<Integer>();
-		}
-		
-		/**
-		 * Parameterized constructor that accepts a list of movers to indlude in this side.
-		 * @param moversToInclude
-		 */
-		public Side(int[] moversToInclude) {
-			
-		}
-
-	}
 
 }
