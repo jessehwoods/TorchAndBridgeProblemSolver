41607654fc04e56e83fd6d9d8560335527de4581
Jesse Woods <jessehwoods@gmail.com>
2022-04-07 10:30:24 -0400

    Started skeleton methods for Side object.

--- TorchAndBridgePuzzleSolver/src/problem/Side.java
+++ TorchAndBridgePuzzleSolver/src/problem/Side.java
@@ -34,5 +34,43 @@ public class Side {
 	public Side(int[] moversToInclude) {
 		
 	}
+	
+	/**
+	 * Moves a mover from this Side to another Side, specified by index.
+	 * @param index is the index of the mover to be moved from this Side to the other.
+	 * @param moveToHere is the Side that is going to have a mover sent to.
+	 */
+	public void moveTo(int index, Side moveToHere) {
+		
+	}
+	
 
+	/**
+	 * Accepts a mover from the provided side and adds it to this Side. 
+	 * @param index is the index in the moveFromHere Side to move
+	 * @param moveFromHere is the Side to move from to this one.
+	 */
+	public void moveFrom(int index, Side moveFromHere) {
+		
+	}
+
+	/**
+	 * Returns the value of the mover stored at an index in the Side.
+	 * @param index index to return.
+	 * @return value stored at the provided index.
+	 */
+	public int get(int index) {
+		
+	}
+	
+	/**
+	 * Returns the number of movers kept in this side.
+	 * @return the number of movers in this Side
+	 */
+	public int size() {
+		
+	}
+	
+	
+	
 }
