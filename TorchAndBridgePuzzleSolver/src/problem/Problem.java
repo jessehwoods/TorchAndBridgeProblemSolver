552ef4a367e52c7438788e6fb6e80dc9ee9bf70b
Jesse Woods <jessehwoods@gmail.com>
2022-04-07 10:17:49 -0400

    Made Side into an inner class to better preserve encapsulation.

--- TorchAndBridgePuzzleSolver/src/problem/Problem.java
+++ TorchAndBridgePuzzleSolver/src/problem/Problem.java
@@ -1,5 +1,8 @@
 package problem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Object holding a problem state for a torch and bridge puzzle. 
  * Movers are stored as ints, representing their speed, stored in arrays, representing the start and end sides of the bridge.
@@ -95,6 +98,40 @@ public class Problem {
 	public enum Location{
 		START_SIDE,
 		TARGET_SIDE;
+	}
+	
+	/**
+	 * Holds information on the sides of the bridge and methods allow the movers to be moved from one
+	 * Side to another.
+	 * Movers on a side are stored as ints representing their speed. 
+	 * Position in the array is used to distinguish different movers with the same speed. 
+	 * 
+	 * @author Jesse Woods
+	 *
+	 */
+	private class Side {
+		
+		/**
+		 * Array holding the movers that are on this side of the bridge. Int value represents 
+		 * the speed of the mover. Kept in ascending order.
+		 */
+		private List<Integer> movers;
+		
+		/**
+		 * Default constructor that creates an empty Side.
+		 */
+		public Side() {
+			this.movers = new ArrayList<Integer>();
+		}
+		
+		/**
+		 * Parameterized constructor that accepts a list of movers to indlude in this side.
+		 * @param moversToInclude
+		 */
+		public Side(int[] moversToInclude) {
+			
+		}
+
 	}
 
 }
