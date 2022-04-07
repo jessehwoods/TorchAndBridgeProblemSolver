9aebb42303cbfb155064ea269216de54da09e946
Jesse Woods <jessehwoods@gmail.com>
2022-04-07 09:52:12 -0400

    Started constructors for Side object.

--- TorchAndBridgePuzzleSolver/src/problem/Side.java
+++ TorchAndBridgePuzzleSolver/src/problem/Side.java
@@ -1,5 +1,8 @@
 package problem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Holds information on the sides of the bridge and methods allow the movers to be moved from one
  * Side to another.
@@ -15,8 +18,21 @@ public class Side {
 	 * Array holding the movers that are on this side of the bridge. Int value represents 
 	 * the speed of the mover. Kept in ascending order.
 	 */
-	private int[] movers;
+	private List<Integer> movers;
+	
+	/**
+	 * Default constructor that creates an empty Side.
+	 */
+	public Side() {
+		this.movers = new ArrayList<Integer>();
+	}
+	
+	/**
+	 * Parameterized constructor that accepts a list of movers to indlude in this side.
+	 * @param moversToInclude
+	 */
+	public Side(int[] moversToInclude) {
+		
+	}
 
-	
-	
 }
